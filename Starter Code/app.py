d9ca5b840e53c49c8a15d5ae8c9857b6278f671c
leandrohugo <125203869+LeandroHugo@users.noreply.github.com>
2023-07-25 18:39:55 -0700

    TCFFPP9C

---
+++ Starter Code/app.py
@@ -0,0 +1,7 @@
+# Import necessary libraries
+import streamlit as st
+from web3 import Web3
+from web3.auto.gethdev import w3
+from solc import compile_source
+from web3.contract import ConciseContract
+
