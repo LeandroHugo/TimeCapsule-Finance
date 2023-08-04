306cc19c9cbf228da8c325d405ba2b49d9bad830
leandrohugo <125203869+LeandroHugo@users.noreply.github.com>
2023-08-04 12:31:43 -0700

    TCFFPP18C

--- Starter_Code/app.py
+++ Starter_Code/app.py
@@ -10,39 +10,6 @@ web3 = Web3(Web3.HTTPProvider(GANACHE_URL))
 # For simplicity, let's use an empty list
 ABI = [
 	{
-		"constant":False,
-		"inputs": [],
-		"name": "deadmanSwitch",
-		"outputs": [],
-		"payable": False,
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"constant": False,
-		"inputs": [],
-		"name": "deposit",
-		"outputs": [],
-		"payable": True,
-		"stateMutability": "payable",
-		"type": "function"
-	},
-	{
-		"constant": False,
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "_lockTime",
-				"type": "uint256"
-			}
-		],
-		"name": "setLockTime",
-		"outputs": [],
-		"payable": False,
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
 		"inputs": [],
 		"payable": False,
 		"stateMutability": "nonpayable",
@@ -74,13 +41,17 @@ ABI = [
 		"type": "event"
 	},
 	{
-		"constant": False,
-		"inputs": [],
-		"name": "withdraw",
-		"outputs": [],
-		"payable": False,
-		"stateMutability": "nonpayable",
-		"type": "function"
+		"anonymous": False,
+		"inputs": [
+			{
+				"indexed": False,
+				"internalType": "uint256",
+				"name": "newLockTime",
+				"type": "uint256"
+			}
+		],
+		"name": "LockTimeUpdated",
+		"type": "event"
 	},
 	{
 		"anonymous": False,
@@ -128,6 +99,30 @@ ABI = [
 		"type": "function"
 	},
 	{
+		"constant": False,
+		"inputs": [
+			{
+				"internalType": "address payable",
+				"name": "newOwner",
+				"type": "address"
+			}
+		],
+		"name": "deadmanSwitch",
+		"outputs": [],
+		"payable": False,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": False,
+		"inputs": [],
+		"name": "deposit",
+		"outputs": [],
+		"payable": True,
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
 		"constant": True,
 		"inputs": [],
 		"name": "lockTime",
@@ -156,9 +151,32 @@ ABI = [
 		"payable": False,
 		"stateMutability": "view",
 		"type": "function"
+	},
+	{
+		"constant": False,
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_lockTime",
+				"type": "uint256"
+			}
+		],
+		"name": "setLockTime",
+		"outputs": [],
+		"payable": False,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": False,
+		"inputs": [],
+		"name": "withdraw",
+		"outputs": [],
+		"payable": False,
+		"stateMutability": "nonpayable",
+		"type": "function"
 	}
 ]
-
 
 
 # The contract address that you get after deploying your smart contract
@@ -196,12 +214,12 @@ if owner == web3.eth.accounts[0]:
         receipt = web3.eth.waitForTransactionReceipt(tx_hash)
         st.write(f"Withdrawal successful. Transaction hash: {receipt['transactionHash'].hex()}")
 
-    # Deadman Switch
-    if st.button('Activate Deadman Switch'):
-        new_owner = st.text_input("Enter new owner address")
+# Deadman Switch
+if st.button('Activate Deadman Switch'):
+    new_owner = st.text_input("Enter new owner address")
+    if Web3.isAddress(new_owner):  # Check if the address is valid
         tx_hash = contract.functions.deadmanSwitch(new_owner).transact({'from': owner})
         receipt = web3.eth.waitForTransactionReceipt(tx_hash)
         st.write(f"Deadman Switch activated. New owner is {new_owner}. Transaction hash: {receipt['transactionHash'].hex()}")
-
-else:
-    st.write("You are not the owner of this contract.")
+    else:
+        st.write("The address entered is not valid. Please enter a valid Ethereum address.")
