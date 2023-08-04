114093eec59a6380d019d981887ea9aeb36b293f
leandrohugo <125203869+LeandroHugo@users.noreply.github.com>
2023-08-04 15:46:38 -0700

    TCFFPP19C

--- Starter_Code/Contracts/contract1.sol
+++ Starter_Code/Contracts/contract1.sol
@@ -2,49 +2,58 @@ pragma solidity ^0.5.0;
 
 contract TimeLockWallet {
     address payable public owner;
-    address payable public backupOwner;
+    address public admin;
+
     uint public balance;
     uint public lockTime;
 
-    event Deposited(address from, uint amount, uint balance);
+    event Deposited(address from, uint amount, uint balance, string message);
     event Withdrawn(address to, uint amount, uint balance);
     event LockTimeUpdated(uint newLockTime);
 
-    constructor(address payable _backupOwner) public {
-        require(_backupOwner != address(0), "Backup owner address can't be zero");
+    constructor() public {
         owner = msg.sender;
-        backupOwner = _backupOwner;
+        admin = msg.sender;  // Initially, the admin is the same as the owner
+    }
+
+    function setAdmin(address _admin) public {
+        require(msg.sender == owner, "Only the owner can set the admin");
+        admin = _admin;
     }
 
     function deposit() public payable {
         require(msg.value > 0, "Must send some ether");
         balance += msg.value;
-        emit Deposited(msg.sender, msg.value, balance);
+        emit Deposited(msg.sender, msg.value, balance, "");
+    }
+
+    function depositWithMessage(string memory message) public payable {
+        require(msg.value > 0, "Must send some ether");
+        balance += msg.value;
+        emit Deposited(msg.sender, msg.value, balance, message);
     }
 
     function setLockTime(uint _lockTime) public {
-        // Only allow setting the lock time when the wallet is created or when it's empty
-        require(balance == 0, "Wallet must be empty to set the lock time");
+        // Only the admin can set the lock time
+        require(msg.sender == admin, "Only the admin can set the lock time");
         lockTime = _lockTime;
         emit LockTimeUpdated(_lockTime);
     }
 
-    function withdraw() public {
-        require(msg.sender == owner || msg.sender == backupOwner, "Only the owner or backup owner can withdraw");
-        require(now >= lockTime, "Wallet is locked");
+function withdraw(uint amount) public {
+    require(msg.sender == owner, "Only the owner can withdraw");
+    require(now >= lockTime, "Wallet is locked");
+    require(amount <= balance, "Insufficient balance");
 
-        uint amount = balance;
-        (bool success, ) = msg.sender.call.value(amount)("");
-        require(success, "Transfer failed.");
-        emit Withdrawn(msg.sender, amount, balance);
-        balance = 0;
-    }
+    balance -= amount;
+    owner.transfer(amount);
+    emit Withdrawn(owner, amount, balance);
+}
 
     function deadmanSwitch(address payable newOwner) public {
+        // Allow the owner to transfer ownership to a backup owner
         require(msg.sender == owner, "Only the owner can use the deadman switch");
-        require(newOwner != address(0), "New owner address can't be zero");
         owner = newOwner;
-        lockTime = 0;  // reset the lock time
     }
 
     function() external payable {
