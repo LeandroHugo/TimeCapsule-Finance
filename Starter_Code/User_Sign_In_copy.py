b6efaa6bebcf7911a2f637c5ba81eabe17d26da2
leandrohugo <125203869+LeandroHugo@users.noreply.github.com>
2023-08-05 13:10:35 -0700

    🦙💬 Llama 2 Chatbot Added

--- Starter_Code/User_Sign_In_copy.py
+++ Starter_Code/User_Sign_In_copy.py
@@ -11,6 +11,7 @@ web3 = Web3(Web3.HTTPProvider(GANACHE_URL))
 ABI = [
 	{
 		"inputs": [],
+		"payable": False,
 		"stateMutability": "nonpayable",
 		"type": "constructor"
 	},
@@ -26,7 +27,7 @@ ABI = [
 			{
 				"indexed": False,
 				"internalType": "uint256",
-				"name": "value",
+				"name": "amount",
 				"type": "uint256"
 			},
 			{
@@ -76,7 +77,7 @@ ABI = [
 			{
 				"indexed": False,
 				"internalType": "uint256",
-				"name": "value",
+				"name": "amount",
 				"type": "uint256"
 			},
 			{
@@ -101,7 +102,7 @@ ABI = [
 			{
 				"indexed": False,
 				"internalType": "uint256",
-				"name": "value",
+				"name": "amount",
 				"type": "uint256"
 			},
 			{
@@ -115,10 +116,12 @@ ABI = [
 		"type": "event"
 	},
 	{
+		"payable": True,
 		"stateMutability": "payable",
 		"type": "fallback"
 	},
 	{
+		"constant": True,
 		"inputs": [],
 		"name": "admin",
 		"outputs": [
@@ -128,37 +131,51 @@ ABI = [
 				"type": "address"
 			}
 		],
+		"payable": False,
 		"stateMutability": "view",
 		"type": "function"
 	},
 	{
+		"constant": True,
 		"inputs": [],
-		"name": "claimOwnership",
-		"outputs": [],
-		"stateMutability": "nonpayable",
+		"name": "balance",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": False,
+		"stateMutability": "view",
 		"type": "function"
 	},
 	{
+		"constant": False,
 		"inputs": [
 			{
-				"internalType": "address",
+				"internalType": "address payable",
 				"name": "newOwner",
 				"type": "address"
 			}
 		],
 		"name": "deadmanSwitch",
 		"outputs": [],
+		"payable": False,
 		"stateMutability": "nonpayable",
 		"type": "function"
 	},
 	{
+		"constant": False,
 		"inputs": [],
 		"name": "deposit",
 		"outputs": [],
+		"payable": True,
 		"stateMutability": "payable",
 		"type": "function"
 	},
 	{
+		"constant": False,
 		"inputs": [
 			{
 				"internalType": "string",
@@ -168,36 +185,12 @@ ABI = [
 		],
 		"name": "depositWithMessage",
 		"outputs": [],
+		"payable": True,
 		"stateMutability": "payable",
 		"type": "function"
 	},
 	{
-		"inputs": [],
-		"name": "inactivityPeriod",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "lastActive",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
+		"constant": True,
 		"inputs": [],
 		"name": "lockTime",
 		"outputs": [
@@ -207,49 +200,57 @@ ABI = [
 				"type": "uint256"
 			}
 		],
+		"payable": False,
 		"stateMutability": "view",
 		"type": "function"
 	},
 	{
+		"constant": True,
 		"inputs": [],
 		"name": "owner",
 		"outputs": [
 			{
-				"internalType": "address",
+				"internalType": "address payable",
 				"name": "",
 				"type": "address"
 			}
 		],
+		"payable": False,
 		"stateMutability": "view",
 		"type": "function"
 	},
 	{
+		"constant": False,
 		"inputs": [
 			{
 				"internalType": "address",
-				"name": "newAdmin",
+				"name": "_admin",
 				"type": "address"
 			}
 		],
 		"name": "setAdmin",
 		"outputs": [],
+		"payable": False,
 		"stateMutability": "nonpayable",
 		"type": "function"
 	},
 	{
+		"constant": False,
 		"inputs": [
 			{
 				"internalType": "uint256",
-				"name": "durationInSeconds",
+				"name": "_lockTime",
 				"type": "uint256"
 			}
 		],
 		"name": "setLockTime",
 		"outputs": [],
+		"payable": False,
 		"stateMutability": "nonpayable",
 		"type": "function"
 	},
 	{
+		"constant": False,
 		"inputs": [
 			{
 				"internalType": "address payable",
@@ -264,10 +265,12 @@ ABI = [
 		],
 		"name": "transfer",
 		"outputs": [],
+		"payable": False,
 		"stateMutability": "nonpayable",
 		"type": "function"
 	},
 	{
+		"constant": False,
 		"inputs": [
 			{
 				"internalType": "uint256",
@@ -277,6 +280,7 @@ ABI = [
 		],
 		"name": "withdraw",
 		"outputs": [],
+		"payable": False,
 		"stateMutability": "nonpayable",
 		"type": "function"
 	}
@@ -405,3 +409,62 @@ Looking forward to seeing you again soon!
 
 # ⚠️ Withdrawal Warning
 st.warning("⚠️ Please be aware that withdrawal transactions may be subject to fees.")  # 💸
+
+
+import streamlit as st
+import replicate
+import os
+
+# App title and sidebar setup
+
+st.sidebar.title('🦙💬 Llama 2 Chatbot')
+st.sidebar.markdown('📖 Learn how to build this app in this [blog](https://blog.streamlit.io/how-to-build-a-llama-2-chatbot/)!')
+
+# Replicate Credentials
+replicate_api = st.secrets.get('REPLICATE_API_TOKEN', '')
+if not replicate_api:
+    replicate_api = st.sidebar.text_input('Enter Replicate API token:', type='password')
+os.environ['REPLICATE_API_TOKEN'] = replicate_api
+
+# Predefined Questions/FAQ
+faq_questions = [
+    "How does the TimeLock feature work?",
+    "What is the Deadman Switch?",
+    "How can I deposit funds into the wallet?"
+]
+
+st.sidebar.header("FAQ")
+selected_question = st.sidebar.selectbox("Choose a question:", [""] + faq_questions)
+if selected_question:
+    # This can be replaced by a function call to get the actual answer in a real-world scenario
+    predefined_answers = {
+        faq_questions[0]: "The TimeLock feature allows you to lock your funds for a specified duration. Once set, funds cannot be withdrawn until the lock period expires.",
+        faq_questions[1]: "The Deadman Switch is a feature that, when activated, transfers the ownership of the wallet to a specified address if the original owner doesn't interact with the wallet for a predefined period.",
+        faq_questions[2]: "To deposit funds, navigate to the User Dashboard and use the deposit function."
+    }
+    answer = predefined_answers.get(selected_question, "Sorry, I don't have an answer for that.")
+
+# Display chat messages
+if "messages" not in st.session_state:
+    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
+
+for message in st.session_state.messages:
+    with st.sidebar.chat_message(message["role"]):
+        st.write(message["content"])
+
+# User input and predefined answer handling
+if selected_question:
+    st.session_state.messages.append({"role": "user", "content": selected_question})
+    with st.sidebar.chat_message("user"):
+        st.write(selected_question)
+    st.session_state.messages.append({"role": "assistant", "content": answer})
+    with st.sidebar.chat_message("assistant"):
+        st.write(answer)
+
+# Clear chat history
+if st.sidebar.button('Clear Chat History'):
+    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
+
+# Main content area remains unchanged for your other app functionalities
+st.write("Main content area for your app...")
+
