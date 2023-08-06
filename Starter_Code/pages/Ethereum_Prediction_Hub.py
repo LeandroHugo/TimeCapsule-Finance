abc155ad398a5bb65cf5f433ff4717c25db71dcd
leandrohugo <125203869+LeandroHugo@users.noreply.github.com>
2023-08-05 22:21:52 -0700

    TCFFPP37C

--- Starter_Code/pages/Ethereum_Prediction_Hub.py
+++ Starter_Code/pages/Ethereum_Prediction_Hub.py
@@ -1,60 +1,54 @@
 import streamlit as st
+import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-from datetime import datetime
-from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
-from sklearn.preprocessing import MinMaxScaler
-import joblib
 
-# Load the trained models
-price_model = joblib.load('eth_rf_model_close.pkl')
-rf_model_complex = joblib.load('rf_model_complex.pkl')
-rf_model_simple = joblib.load('rf_model_simple.pkl')
+# ... [all the previous imports and functions] ...
 
-# Load the data
-df = pd.read_csv('Eth_USD_18_23.csv')
+def eth_prediction_model():
+    # Placeholder for your Ethereum prediction model
+    # For demonstration, I'll return a random prediction
+    return np.random.randint(2000, 4000)
 
-# Function to preprocess the input data for price prediction
-def preprocess_input(date: datetime, df: pd.DataFrame) -> np.array:
-    day = date.day
-    scaler = MinMaxScaler()
-    day = scaler.fit_transform(np.array(day).reshape(-1, 1))
-    return day
+def plot_eth_graph():
+    # Placeholder for your Ethereum graph
+    # For demonstration, I'll plot a random graph
+    df = pd.DataFrame({
+        'Date': pd.date_range(start='1/1/2022', periods=30),
+        'Price': np.random.randint(2000, 4000, size=(30))
+    })
+    plt.figure(figsize=(10, 5))
+    plt.plot(df['Date'], df['Price'])
+    plt.title('Ethereum Price Over Time')
+    plt.xlabel('Date')
+    plt.ylabel('Price')
+    plt.grid(True)
+    st.pyplot()
 
-def predict_eth_price():
-    st.title('ETH Price Prediction 📈')
-    st.write("Use this tool to predict the closing price of Ethereum (ETH) for a specific date.")
-    date_to_predict = st.date_input("Choose a date", min_value=datetime.today())
-    if st.button("Predict"):
-        input_data = preprocess_input(date_to_predict, df)
-        prediction = price_model.predict(input_data)
-        st.success(f"The predicted closing price for Ethereum on {date_to_predict} is ${prediction[0]:.2f}.")
-
-def predict_eth_trend():
-    st.title('ETH Price Trend Prediction 📉📈')
-    st.write("Use this tool to predict the price trend of Ethereum (ETH) for a specific date.")
-    # Assuming latest_data fetch and preprocessing is done here...
-    pred_complex, pred_simple = get_ethereum_predictions(latest_data)
-    st.subheader('Predicted Ethereum Trend:')
-    if pred_complex[0] == 1:
-        st.write("Based on complex model: Expected Upward Trend")
-    else:
-        st.write("Based on complex model: Expected Downward Trend")
-    if pred_simple[0] == 1:
-        st.write("Based on simple model: Expected Upward Trend")
-    else:
-        st.write("Based on simple model: Expected Downward Trend")
-
-# Main app
 def main():
     st.sidebar.title("Navigation")
-    selection = st.sidebar.radio("Go to", ["Home", "ETH Price Prediction", "ETH Trend Prediction"])
+    pages = ["Home", "Photo Upload Widget", "Streamlit Elements Demo", "Llama 2 Chatbot"]
+    selection = st.sidebar.radio("Choose Page", pages)
+
     if selection == "Home":
-        st.write("# Home Page")
-    elif selection == "ETH Price Prediction":
-        predict_eth_price()
-    elif selection == "ETH Trend Prediction":
-        predict_eth_trend()
+        st.title("Home Page")
+        
+        # Introduction
+        st.write("""
+        Welcome to our Streamlit application! Here you will find various tools and insights 
+        related to cryptocurrency, especially Ethereum. Below is our latest Ethereum price prediction 
+        and its historical trend over the past month.
+        """)
+
+        # Ethereum Prediction
+        prediction = eth_prediction_model()
+        st.write(f"🔮 Ethereum Price Prediction: ${prediction}")
+
+        # Ethereum Graph
+        st.write("### Ethereum Price Trend Over the Last Month:")
+        plot_eth_graph()
+
+    # ... [rest of the page sections] ...
 
 if __name__ == "__main__":
     main()
